d1f36de9783b6c67e3649f0c7338be1f8859e0fc
ByunDohwi <162133680+ByunDohwi@users.noreply.github.com>
2024-03-25 23:06:30 +0900

    [Bronze I] Title: 단어 공부, Time: 16 ms, Memory: 1972 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.c
+++ 백준/Bronze/1157. 단어 공부/단어 공부.c
@@ -2,31 +2,32 @@
 #include <string.h>
 int main()
 {
-    char arr[1000000];
-    int result[30] = { 0 }, answer = 0, temp = 0, max =0, cnt=0, index = 0;
-    scanf("%s", arr);
-    for (int i = 0; arr[i] != '\0'; i++)
-    {
-        if (arr[i] >= 97)
-            arr[i] -= 32;
-        result[arr[i] - 'A']++;
-    }
-    for (int i = 0; i < 26; i++)
-    {
-        if (temp < result[i])
-        {
-            temp = result[i];
-            max = result[i];
-            index = i;
-        }
-    }
-    for (int i = 0; i < 26; i++)
-    {
-        if (max == result[i])
-            cnt++;
-    }
-    if (cnt > 1)
-        printf("?");
-    else
-        printf("%c", index + 'A');
+	char arr[1000000];
+	int result[30] = { 0 }, answer = 0, temp = 0, max =0, cnt=0, index = 0;
+	scanf("%s", &*arr);
+    int len = strlen(arr);
+	for (int i = 0; i < len; i++)
+	{
+		if (arr[i] >= 97)
+			arr[i] -= 32;
+		result[arr[i] - 'A']++;
+	}
+	for (int i = 0; i < 26; i++)
+	{
+		if (temp < result[i])
+		{
+			temp = result[i];
+			max = result[i];
+			index = i;
+		}
+	}
+	for (int i = 0; i < 26; i++)
+	{
+		if (max == result[i])
+			cnt++;
+	}
+	if (cnt > 1)
+		printf("?");
+	else
+		printf("%c", index + 'A');
 }
