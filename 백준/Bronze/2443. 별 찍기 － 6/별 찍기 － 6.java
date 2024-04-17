336b91fe415098f35d9a9ffbb9196c99aebc5a90
ByunDohwi <162133680+ByunDohwi@users.noreply.github.com>
2024-04-17 11:33:25 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 212 ms, Memory: 17692 KB -BaekjoonHub

--- 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -1,20 +1,27 @@
-import java.util.*;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
 class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
-
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = scanner.nextInt();
-
+        String star = "*";
+        String t = " ";
         int temp = 0;
         for(int i = (n*2)-1; i > 0; i -= 2){
             for (int j = 0; j<temp ; j++) {
-                System.out.printf(" ");
+                bw.write(t);
             }
             for (int j = 0; j<i ; j++) {
-                System.out.printf("*");
+                bw.write(star);
             }
             temp++;
-            System.out.println("");
+            bw.newLine();
         }
+        bw.flush();
+        bw.close();
     }
 }
