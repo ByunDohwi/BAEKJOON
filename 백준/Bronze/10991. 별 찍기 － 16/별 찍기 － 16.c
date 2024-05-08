2f4a57329231e46bc800b2e528850eab38896886
ByunDohwi <162133680+ByunDohwi@users.noreply.github.com>
2024-05-08 10:45:31 +0900

    [Bronze III] Title: 별 찍기 - 16, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10991. 별 찍기 － 16/별 찍기 － 16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+	int a;
+	scanf("%d", &a);
+	for (int i = 1; i <= a; i++)
+	{
+		for (int j = a - i; j > 0; j--)
+		{
+			printf(" ");
+		}
+		for (int j = 0; j < i; j++) {
+			printf("*");
+			if(j != a)
+			{
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+}
